50debe9f5d8555031ba4fb90b01ab70c16357a23
Pranita Jain B <letskod@gmail.com>
2024-09-09 18:09:11 +0530

    Hero section added

--- ContactUsForm/src/App.jsx
+++ ContactUsForm/src/App.jsx
@@ -3,15 +3,18 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './component/Header/Header';
+import Hero from './component/Hero/Hero';
 
 
 function App() {
   const [count, setCount] = useState(0)
-
   return (
-    
+  <div className="pagecolor">
+
       <Header/>
-    
+      <Hero/>
+   
+  </div>
   )
 }
 
---
+++ ContactUsForm/src/component/Hero/Hero.jsx
@@ -0,0 +1,45 @@
+import style from "./Hero.module.css";
+
+const Hero = () => {
+  return (
+    <div className={style.hero}>
+      <div className={style.formContainer}>
+        <div className={style.formHeading}>
+          <div className={style.formTitle}>Let’s connect constellations</div>
+          <div className={style.formSubtitle}>
+            Let's align our constellations! Reach out and let the magic of
+            collaboration illuminate our skies.
+          </div>
+        </div>
+        <div className={style.formBody}>
+          <div className={style.name}>
+            <input className={style.fname}  placeholder="First Name">
+                
+                </input>
+                <input className={style.lname}  placeholder="Last Name">
+                
+                </input>
+          </div>
+          <input className={style.email} placeholder="Email"></input>
+          <input className={style.phone} placeholder="Phone"></input>
+          <input className={style.message} placeholder="Message"></input>
+          <button className={style.submit} >Send it to the moon <img src="./images/rocket.png" className={style.rocketImg}  alt="Button Image" /></button>
+
+        </div>
+      </div>
+      <div className={style.imageContainer}>
+        <img src="./images/astro.png" alt="img here" className={style.imageAstro}/>
+        <div ></div>
+        <div className={style.imageDesc}>
+          <div className={style.imageDescQuote}>
+            “Two lunar months revealed Earth's fragile beauty against vast
+            silence, transforming my view of our place in the universe."
+          </div>
+          <div className={style.imageDescAuthor}>Irinel Traista</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
