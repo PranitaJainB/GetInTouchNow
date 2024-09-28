888ff4e3cb00e7de9a6275cac9e5f5202de1e412
Pranita Jain B <letskod@gmail.com>
2024-09-28 12:43:44 +0530

    Button component created

--- ContactUsForm/src/component/Hero/Hero.jsx
+++ ContactUsForm/src/component/Hero/Hero.jsx
@@ -1,6 +1,18 @@
 import style from "./Hero.module.css";
+import { IoChatboxEllipsesOutline } from "react-icons/io5";
+import { IoMdCall } from "react-icons/io";
+import { RxRocket } from "react-icons/rx";
+import Button from "../Button/Button";
 
 const Hero = () => {
+  const onBtnClick = (btnName) => {
+   
+    console.log(`User clicked ${btnName} button, this is from Child value`);
+  };
+  const parentLog = () => {
+    console.log(`User clicked button, this is from Parent,no value`);
+  };
+
   return (
     <div className={style.hero}>
       <div className={style.formContainer}>
@@ -19,14 +31,16 @@ const Hero = () => {
           <input className={style.email} placeholder="Email"></input>
           <input className={style.phone} placeholder="Phone"></input>
           <input className={style.message} placeholder="Message"></input>
-          <button className={style.submit}>
-            Send it to the moon{" "}
-            <img
-              src="./images/rocket.png"
-              className={style.rocketImg}
-              alt="Button Image"
+          <Button name="submitButton" text="Submit" icon={<RxRocket />} btnPrimary={true} onBtnClick={onBtnClick}  />
+          <div className={style.name}>
+            <Button
+            name="chatButton"
+              text="Chat"
+              icon={<IoChatboxEllipsesOutline />}
+              onBtnClick={onBtnClick}
             />
-          </button>
+            <Button name="callButton" text="Call" icon={<IoMdCall />} onBtnClick={onBtnClick} />
+          </div>
         </div>
       </div>
       <div className={style.imageContainer}>
@@ -41,7 +55,7 @@ const Hero = () => {
             “Two lunar months revealed Earth's fragile beauty against vast
             silence, transforming my view of our place in the universe."
           </div>
-          <div className={style.imageDescAuthor}>Irinel Traista</div>
+          <div className={style.imageDescAuthor}>~ Irinel Traista</div>
         </div>
       </div>
     </div>
